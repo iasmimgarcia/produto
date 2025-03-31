8519b62c0a92a1d7493516ee502f9c070ca56531
iasmimgarcia <igarciacastrodev@gmail.com>
2025-03-31 09:31:12 -0300

    Aula2703

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+insert into tb_category (name) values ('discos');
+insert into tb_category (name) values ('livros');
+insert into tb_category (name) values ('computadores');
+insert into tb_category (name) values ('eletrônicos');
+insert into tb_category (name) values ('dvds');
+insert into tb_category (name) values ('cds');
